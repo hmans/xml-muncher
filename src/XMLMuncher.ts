275c668ae54a226b9db00b2a2380dd4504379258
Hendrik Mans <hendrik@mans.de>
2023-07-06 19:12:50 +0200

    Cleanup

--- src/XMLMuncher.ts
+++ src/XMLMuncher.ts
@@ -31,8 +31,8 @@ export class XMLMuncher extends EventEmitter {
       currentElement = {};
 
       /* Add attributes to the element */
-      for (const [key, value] of Object.entries(attributes)) {
-        currentElement[`$${key}`] = value;
+      for (const key in attributes) {
+        currentElement[`$${key}`] = attributes[key];
       }
     });
 
@@ -42,29 +42,33 @@ export class XMLMuncher extends EventEmitter {
 
       /* If the element only contains a text attribute and nothing else,
       let's turn it into a string. */
-      if (Object.keys(newElement).length === 1 && newElement["#text"]) {
+      if (Object.keys(newElement).length === 1 && "#text" in newElement) {
         newElement = newElement["#text"];
       }
 
       /* Sort the new element into the current element */
       if (currentElement[element] === undefined) {
+        /* Nothing of the name exists yet, so we can just set it. */
         currentElement[element] = newElement;
       } else if (Array.isArray(currentElement[element])) {
+        /* The element of this name is an array, so let's add the new element to that. */
         currentElement[element].push(newElement);
       } else {
+        /* Something is already using the name, so let's turn it into an array. */
         currentElement[element] = [currentElement[element], newElement];
       }
 
-      /* Generate the path for this element */
-      const path = "//" + nameStack.join("/");
-      nameStack.pop();
-
-      /* Emit events */
+      /* Emit element event */
       this.emit(`element:${element}`, newElement);
-      this.emit(`path:${path}`, newElement);
+
+      /* Emit path event */
+      this.emit(`path:${"//" + nameStack.join("/")}`, newElement);
+      nameStack.pop();
     });
 
     this.parser.on("text", (text: string) => {
+      /* There's going to be a lot of text nodes with just spaces and newlines,
+      so let's just ignore those. */
       if (text.trim() === "") return;
 
       currentElement["#text"] = text;
