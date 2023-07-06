57449cbf66548d09916b78eab0ba3508f9765781
Hendrik Mans <hendrik@mans.de>
2023-07-06 19:39:47 +0200

    Use describe blocks

--- test/XMLMuncher.test.ts
+++ test/XMLMuncher.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { XMLMuncher } from "../src/XMLMuncher";
 
 async function extractElements(xml: string, query: string) {
@@ -29,55 +29,59 @@ const given = (xml: string) => ({
   }),
 });
 
-test("Basic empty elements", () =>
-  given("<foo />").query("element:foo").expect([{}]));
+describe("parsing", () => {
+  test("Basic empty elements", () =>
+    given("<foo />").query("element:foo").expect([{}]));
 
-test("Elements with text", () =>
-  given("<foo>foo</foo>").query("element:foo").expect(["foo"]));
+  test("Elements with text", () =>
+    given("<foo>foo</foo>").query("element:foo").expect(["foo"]));
 
-test("Elements with nested children", () =>
-  given("<foo><bar>Hello World</bar></foo>")
-    .query("element:foo")
-    .expect([{ bar: "Hello World" }]));
+  test("Elements with nested children", () =>
+    given("<foo><bar>Hello World</bar></foo>")
+      .query("element:foo")
+      .expect([{ bar: "Hello World" }]));
 
-test("Directly selecting the child", () =>
-  given("<foo><bar>Hello World</bar></foo>")
-    .query("element:bar")
-    .expect(["Hello World"]));
+  test("Directly selecting the child", () =>
+    given("<foo><bar>Hello World</bar></foo>")
+      .query("element:bar")
+      .expect(["Hello World"]));
 
-test("Multiple children of the same name", () =>
-  given("<items><item>One</item><item>Two</item></items>")
-    .query("element:items")
-    .expect([{ item: ["One", "Two"] }]));
+  test("Multiple children of the same name", () =>
+    given("<items><item>One</item><item>Two</item></items>")
+      .query("element:items")
+      .expect([{ item: ["One", "Two"] }]));
 
-test("Mixed text and element children, wHo doEs tHiS?", () =>
-  given("<foo>Hello<bar>World</bar></foo>")
-    .query("element:foo")
-    .expect([{ "#text": "Hello", bar: "World" }]));
+  test("Mixed text and element children, wHo doEs tHiS?", () =>
+    given("<foo>Hello<bar>World</bar></foo>")
+      .query("element:foo")
+      .expect([{ "#text": "Hello", bar: "World" }]));
 
-test("Attributes", () =>
-  given('<foo bar="baz" />')
-    .query("element:foo")
-    .expect([{ $bar: "baz" }]));
+  test("Attributes", () =>
+    given('<foo bar="baz" />')
+      .query("element:foo")
+      .expect([{ $bar: "baz" }]));
 
-test("Attributes with text", () =>
-  given('<foo bar="baz">Foo</foo>')
-    .query("element:foo")
-    .expect([{ "#text": "Foo", $bar: "baz" }]));
+  test("Attributes with text", () =>
+    given('<foo bar="baz">Foo</foo>')
+      .query("element:foo")
+      .expect([{ "#text": "Foo", $bar: "baz" }]));
 
-test("Attributes with nested children", () =>
-  given('<foo bar="baz"><child>Child</child></foo>')
-    .query("element:foo")
-    .expect([{ $bar: "baz", child: "Child" }]));
+  test("Attributes with nested children", () =>
+    given('<foo bar="baz"><child>Child</child></foo>')
+      .query("element:foo")
+      .expect([{ $bar: "baz", child: "Child" }]));
+});
 
-/* Error handling */
+describe("error handling", () => {
+  test("Invalid XML", () => given("<foo").query("element:foo").expect([]));
 
-test("Invalid XML", () => given("<foo").query("element:foo").expect([]));
+  test("Wrongly closed tags", () =>
+    given("<foo><bar></foo>")
+      .query("element:foo")
+      .expectError("mismatched tag"));
 
-test("Wrongly closed tags", () =>
-  given("<foo><bar></foo>").query("element:foo").expectError("mismatched tag"));
-
-test("Invalid characters", () =>
-  given("<💩foo>foo</💩foo>")
-    .query("element:foo")
-    .expectError("not well-formed (invalid token)"));
+  test("Invalid characters", () =>
+    given("<💩foo>foo</💩foo>")
+      .query("element:foo")
+      .expectError("not well-formed (invalid token)"));
+});
