9f6deab71d937fdcb7e39dc120c1e0f3a7e4f6b0
Hendrik Mans <hendrik@mans.de>
2023-07-06 16:37:58 +0200

    It's a start

--- src/XMLMuncher.ts
+++ src/XMLMuncher.ts
@@ -2,6 +2,8 @@ import { createReadStream } from "node:fs";
 import expat from "node-expat";
 import { buffer } from "stream/consumers";
 
+type Element = Record<any, any>;
+
 export class XMLMuncher {
   public parser = new expat.Parser("UTF-8");
 
@@ -16,15 +18,34 @@ export class XMLMuncher {
   }
 
   async munchFile(filePath: string) {
-    let count = 0;
+    let currentElement: Element | Element[] = {};
+    const stack = new Array<Element>();
 
     this.parser.on("startElement", (element, attributes) => {
-      if (element === "job") {
-        console.log(count++);
+      // console.debug("startElement", element, attributes);
+
+      /* Create a fresh element, full of hopes and dreams */
+      const newElement: Element = {};
+
+      if (Array.isArray(currentElement[element])) {
+        currentElement[element].push(newElement);
+      } else if (currentElement[element]) {
+        currentElement[element] = [currentElement[element], newElement];
+      } else {
+        currentElement[element] = newElement;
       }
+
+      stack.push(currentElement);
+
+      currentElement = newElement;
     });
 
-    this.parser.on("endElement", (element) => {});
+    this.parser.on("endElement", (element) => {
+      // console.debug("endElement", element);
+      currentElement = stack.pop()!;
+
+      if (element === "job") console.dir(currentElement, { depth: null });
+    });
 
     const input = createReadStream(filePath, {
       encoding: "utf-8",
