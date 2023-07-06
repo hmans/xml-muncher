3e90d14da2e2c13182451382d408a77065a8d285
Hendrik Mans <hendrik@mans.de>
2023-07-06 17:36:47 +0200

    More tests

--- test/XMLMuncher.test.ts
+++ test/XMLMuncher.test.ts
@@ -26,15 +26,29 @@ const extract = (xml: string) => ({
 });
 
 describe(XMLMuncher, () => {
+  /* Basic empty elements */
+  extract("<foo />").query("element:foo").expect([{}]);
+
+  /* Elements with text */
+  extract("<foo>foo</foo>").query("element:foo").expect(["foo"]);
+
+  /* Elements with nested children */
   extract("<foo><bar>Hello World</bar></foo>")
     .query("element:foo")
     .expect([{ bar: "Hello World" }]);
 
+  /* Directly selecting the child */
   extract("<foo><bar>Hello World</bar></foo>")
     .query("element:bar")
     .expect(["Hello World"]);
 
+  /* Multiple children of the same name */
   extract("<items><item>One</item><item>Two</item></items>")
     .query("element:items")
     .expect([{ item: ["One", "Two"] }]);
+
+  /* Mixed text and element children, wHo doEs tHiS? */
+  extract("<foo>Hello<bar>World</bar></foo>")
+    .query("element:foo")
+    .expect([{ "#text": "Hello", bar: "World" }]);
 });
